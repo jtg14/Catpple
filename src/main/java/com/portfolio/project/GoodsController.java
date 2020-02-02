0af6e23fb17272e673bf1c0b0e1b4db5000d7eff
Arsyu <60072792+Arsyu@users.noreply.github.com>
2020-02-02 15:09:11 +0900

    sellerGoodsForm -0202 3시
    파일경로 변경 및 Insert추가함

--- src/main/java/com/portfolio/project/GoodsController.java
+++ src/main/java/com/portfolio/project/GoodsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import business.GService;
 import vo.GoodsVO;
+import vo.MemberVO;
 
 @Controller
 public class GoodsController {
@@ -21,17 +22,18 @@ public class GoodsController {
 	GService service;
 	
 	@RequestMapping(value = "goodsInsert", method = RequestMethod.POST)
-	public ModelAndView goodsInsert(ModelAndView mv, GoodsVO vo) {
-		String uploadPath = "C:\\Catpple\\src\\main\\webapp\\resources\\images\\"+vo.getMid();
+	public ModelAndView goodsInsert(HttpServletRequest request, ModelAndView mv, GoodsVO vo) {
+		MemberVO mvo = new MemberVO();
+		mvo = (MemberVO)request.getSession().getAttribute("logInUser");
+		vo.setMid(mvo.getmId());
+		String uploadPath = "C:\\Catpple\\src\\main\\webapp\\resources\\sellerInfo\\"+vo.getMid();
 		MultipartFile multipartFile1 = vo.getGimgf1();
 		MultipartFile multipartFile2 = vo.getGimgf2();
 		File file1 = new File(uploadPath, multipartFile1.getOriginalFilename());
 		File file2 = new File(uploadPath, multipartFile2.getOriginalFilename());
 		
-		vo.setGimg1("resources/images/"+multipartFile1.getOriginalFilename());
-		vo.setGimg2("resources/images/"+multipartFile2.getOriginalFilename());
-		vo.setGimg1("resources/images/"+vo.getMid()+"/"+multipartFile1.getOriginalFilename());
-		vo.setGimg2("resources/images/"+vo.getMid()+"/"+multipartFile2.getOriginalFilename());
+		vo.setGimg1(multipartFile1.getOriginalFilename());
+		vo.setGimg2(multipartFile2.getOriginalFilename());
 		int cnt = service.goodsInsert(vo);
 		if(cnt>0) {
 			if(!multipartFile1.isEmpty() && !multipartFile2.isEmpty()) {
